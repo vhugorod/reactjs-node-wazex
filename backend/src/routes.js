f1f127d31da19c4ca7e0dba24da6108dae3b7361
Vitor Rodrigues <vitorrodrigueshr10@gmail.com>
2020-01-16 21:11:05 -0300

    route creations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,27 +1,12 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs', async (request, response) => {
-  const { github_username, techs } = request.body;
-  
-  const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-  const { name = login, avatar_url, bio } = apiResponse.data;
-
-  const techsArray = techs.split(',').map(tech => tech.trim());
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray,
-  });
-
-  return response.json(dev);
-});
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
